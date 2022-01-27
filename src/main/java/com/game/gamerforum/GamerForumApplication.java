12521a6cb79f2d8fa48dbfe00969be635c8a967c
George Tudor <Alexander.George.Tudor@gmail.com>
2022-01-27 00:02:31 -0600

    first update

---
+++ src/main/java/com/game/gamerforum/GamerForumApplication.java
@@ -0,0 +1,13 @@
+package com.game.gamerforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamerForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/game/gamerforum/GamerForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.gamerforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
