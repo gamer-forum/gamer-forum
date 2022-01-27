6958607a18bead55fed008413b0a32e467013d9a
George Tudor <Alexander.George.Tudor@gmail.com>
2022-01-27 15:59:35 -0600

    setup vue and connection to backend

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <hello-world/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
